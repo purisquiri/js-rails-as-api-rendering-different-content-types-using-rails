72b30c7156ef0efc03516207988917a4d6e21b96
purisquiri <mau.dorigo@outlook.es>
2020-11-28 16:22:03 -0500

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,9 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
+    birds = Bird.all
+    # render 'birds/index.html.erb'
+    # render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }
+    # rather than an array with four birds, adding a message we'll alter the structure an instead will have an object with two keys (critical concept to consider)
+    render json: birds #we dont need to instanciate the variable (@birds)
   end
 end
